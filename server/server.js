4047accf5b1c30028369bf7969f7cbafd5c0be03
Ironmac17 <nimish4agrawal@gmail.com>
2025-08-11 17:13:50 +0530

    Depoly

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/db.js"
 import * as Sentry from "@sentry/node";
+import { clerbWebhooks } from './controllers/webhooks.js'
 
 //express
 const app = express()
@@ -23,6 +24,8 @@ app.get('/', (req, res) => {
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
+
+app.post('/webhooks',clerbWebhooks)
 
 //port
 const PORT = process.env.PORT || 5000
